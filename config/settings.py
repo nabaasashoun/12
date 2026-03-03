7d68c8f66188ce8e7c37053edc0261d344653ea3
nabaasashoun <nabaasashoun@gmail.com>
2026-03-04 00:33:59 +0300

    added star on allowed hosts

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-)b!6d2pvoz9o=t#$jk)e&7*uiye-(p(nxfs@d_o*)giup9!*bw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
