8e8918e39df3024db030f7e95df0250ad3ca87a3
nabaasashoun <nabaasashoun@gmail.com>
2026-06-26 00:45:47 +0300

    Add database packages

--- config/settings.py
+++ config/settings.py
@@ -1,6 +1,7 @@
 # settings.py - FIXED (without allauth)
 
 from pathlib import Path
+import dj_database_url
 import os
 from datetime import timedelta
 
@@ -95,10 +96,9 @@ CHANNELS_DEBUG = False
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 
