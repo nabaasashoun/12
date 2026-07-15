690a898bf37b1e34ac129e207843844916d7142e
nabaasashoun <nabaasashoun@gmail.com>
2026-07-15 22:21:10 +0300

    settings.py

--- config/settings.py
+++ config/settings.py
@@ -103,9 +103,8 @@ CHANNELS_DEBUG = False
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
     )
 }
 
